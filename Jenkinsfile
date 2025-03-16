fb656f66280f2675f4459977edabaff57942e3d1
manupanand <manupanandindianinvestor@gmail.com>
2025-03-16 18:29:36 +0530

    githubactions-repo-commit

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,5 @@
 
 @Library('roboshop-jenkins-library') _  // declaring libray
+env.appName="ecomm-cart"
 env.appType="nodejs"// sending a paramter to identify type|sice we dotn want to run compile for node
 ci_pipeline() //calling the pipeline-ci-jenkins-prjt-infra
---
+++ run.sh
@@ -0,0 +1,21 @@
+# to make sure that environment variables are present before running image
+
+
+if [ -z "${REDIS_HOST}"]; then
+    echo Input REDIS_HOST is missing
+    exit 1
+fi
+
+
+if [ -z "${CATALOGUE_HOST}"]; then
+    echo Input CATALOGUE_HOST is missing
+    exit 1
+fi
+
+
+if [ -z "${CATALOGUE_PORT}"]; then
+    echo Input CATALOGUE_PORT is missing
+    exit 1
+fi
+
+node  /app/server.js
