258e8d6433bedc65b47be7daff6a64212a7f7e73
manupanand <manupanandindianinvestor@gmail.com>
2025-06-03 15:09:50 +0530

    updated

--- run.sh
+++ run.sh
@@ -2,19 +2,19 @@
 # to make sure that environment variables are present before running image
 
 
-if [ -z "${REDIS_HOST}"]; then
+if [ -z "${REDIS_HOST}" ]; then
     echo Input REDIS_HOST is missing
     exit 1
 fi
 
 
-if [ -z "${CATALOGUE_HOST}"]; then
+if [ -z "${CATALOGUE_HOST}" ]; then
     echo Input CATALOGUE_HOST is missing
     exit 1
 fi
 
 
-if [ -z "${CATALOGUE_PORT}"]; then
+if [ -z "${CATALOGUE_PORT}" ]; then
     echo Input CATALOGUE_PORT is missing
     exit 1
 fi
